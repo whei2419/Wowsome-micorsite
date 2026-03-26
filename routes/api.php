cbff48404891f79f99e08a937c363e735ff88e63
whei2419 <87625823+whei2419@users.noreply.github.com>
2026-03-27 05:10:00 +0800

    fix route issue

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UploadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+    Route::post('/uploads', [UploadController::class, 'store']);
+    Route::get('/uploads/latest', [UploadController::class, 'latest']);
 });
